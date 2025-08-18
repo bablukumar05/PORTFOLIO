cfafbdc4411a498a16ec96a123370868d6f973d6
bablukumar05 <kumarbablu.com@gmail>
2025-08-18 16:40:49 +0530

    Update Vite config and package files for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/PORTFOLIO/', // <-- Add this
     plugins: [react()],
-    base: "/My-Portofolio/", // must match EXACT repo name (case-sensitive)
 });
