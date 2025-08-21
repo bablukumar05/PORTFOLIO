5525cedd9300f031a99cd72f2bc2307be8804fc5
bablukumar05 <kumarbablu.com@gmail>
2025-08-21 13:11:41 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: "/PORTFOLIO/", // 👈 very important for GitHub Pages
-    plugins: [react()],
-})
